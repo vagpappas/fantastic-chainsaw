65de8af8fd5f8c1d05362e5651b12ccba825cc5a
EfiGavr <98419370+EfiGavr@users.noreply.github.com>
2022-05-05 19:35:09 +0300

    efi's commit

--- src/github/Github.java
+++ src/github/Github.java
@@ -11,6 +11,8 @@ public class Github {
     
     public static void main(String[] args) {
         System.out.println("Hello World");
+        System.out.println("I'm hungry again");
+
     }
     
 }
